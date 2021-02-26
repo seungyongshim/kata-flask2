edad171f1a4d5c8677709acacade188ee62301eb
심승용 <seungyongshim@nexon.co.kr>
2021-02-26 15:01:28 +0900

    flask 기본 코드 추가

---
+++ src/app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from datetime import datetime
+import re
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+
+@app.route("/hello/<name>")
+def hello_there(name: str):
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d, %B, %Y at %X")
+    match_object = re.match("[a-zA-Z]+", name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+
+    content = "Hello there," + clean_name + "! It's " + formatted_now
+    return content
